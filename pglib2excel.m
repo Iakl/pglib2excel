72d55066c8244fee485bb97de7469d7845e6de8e
Esteban Gonzalez Iakl <1207326+Iakl@users.noreply.github.com>
2024-11-20 14:49:40 +0100

    Updated to MVEMS v1.03

--- pglib2excel.m
+++ pglib2excel.m
@@ -10,32 +10,28 @@ err = 0;
 
 define_constants;
 %% Scenario base configuration 
-config_headers = {'nper', 'pdur', 'sn_mva'};
-nper = 1; % By default, 1 period
-pdur = 60; % By default, 60 minutes of period duration
 sn = mpc.baseMVA; % Base MVA for all system
 if ~all(mpc.gen(:,MBASE) == sn)
     fprintf('WARNING, DIFFERENT BASE MVA DEFINED IN SCENARIO FOR GENERATORS, USING BASE %i MVA\n', sn)
 end
-config_data = {nper, pdur, sn};
 
 %% Bus data creation
-bus_headers = {'index', 'slack', 'gs_mw', 'bs_mvar', 'vn_kv', 'v_pu_min', 'v_pu_max', 'ang_grad_min', 'ang_grad_max'};
+bus_headers = {'index', 'alias', 'slack', 'gs_mw', 'bs_mvar', 'vn_kv', 'v_pu_min', 'v_pu_max'};
 % bus_i = mpc.bus(:,BUS_I) - 1;
 bus_i = (0:(height(mpc.bus) - 1)).';
-bus_name = mpc.bus(:,BUS_I);
+alias = mpc.bus(:,BUS_I);
 bus_slack = zeros(height(mpc.bus), 1) + (mpc.bus(:, BUS_TYPE) == 3);
-ang_grad_min = zeros(height(mpc.bus), 1) - 180; % By default -180
-ang_grad_max = zeros(height(mpc.bus), 1) + 180; % By default 180
+gs = mpc.bus(:,GS);
+bs = mpc.bus(:,BS);
 % pglib bus data structure:
 %	1bus_i	2type	3Pd	4Qd	5Gs	6Bs	7area	8Vm	9Va	10baseKV	11zone	12Vmax	13Vmin
-bus_data = [bus_i bus_slack mpc.bus(:,[GS BS BASE_KV VMIN VMAX]) ang_grad_min ang_grad_max];
+bus_data = [bus_i alias bus_slack gs bs mpc.bus(:,[BASE_KV VMIN VMAX])];
 
 %% Generators data creation
-gen_headers = {'index', 'bus', 'p_mw_ini', 'p_mw', 'q_mvar', 'p_mw_min', 'p_mw_max', 'q_mvar_min', 'q_mvar_max', 'is_active', 'v_pu_set', 'p_c2', 'p_c1', 'q_c2', 'q_c1', 'price_mode', 'controllable', 'priority', 's_max_mode', 'd_mw_max'};
+gen_headers = {'index', 'connection1', 'p_mw_ini', 'p_mw', 'q_mvar', 'p_mw_min', 'p_mw_max', 'q_mvar_min', 'q_mvar_max', 'is_active', 'v_pu_set', 'p_c2', 'p_c1', 'q_c2', 'q_c1', 'controllable', 'priority', 'd_mw_max'};
 gen_i = (0:(height(mpc.gen) - 1)).';
 % gen_name = "gen" + gen_i;
-gen_bus = mpc.gen(:, GEN_BUS) - 1;
+gen_bus = mpc.gen(:, GEN_BUS);
 % gen_bus = find(ismember(bus_name, mpc.gen(:, GEN_BUS)));
 if ~all(mpc.gencost(:, MODEL) == 2)
     fprintf('ERROR, PIECEWISE COST MODEL DEFINED. THIS WORKS ONLY FOR POLYNOMIALS\n')
@@ -101,24 +97,20 @@ if height(slack_i) > 1
 end
 p_mw_ini = zeros(height(mpc.gen), 1); % All generators start at P = 0 by default
 v_set = zeros(height(mpc.gen), 1) + (mpc.gen(:, GEN_BUS)==slack_i).*mpc.gen(:,VG); % By default generators doesnt fix voltage magnitude (assumes opf.use_vg = 0 in matpower)
-price_mode = "fixed" + strings(height(mpc.gen), 1); % By default all generators use fixed costs
 controllable = ones(height(mpc.gen), 1); % By default all generators are controllable
 priority = ones(height(mpc.gen), 1); % By default all generators have priority 1
-s_max_mode = "fixed" + strings(height(mpc.gen), 1); % By default all generators use fixed maximum
 d_mw_max = ones(height(mpc.gen), 1) * max(mpc.gen(:, PMAX))*10; % By default ramps are not constraints
 % pglib gen data structure:
 %	1bus	2Pg	3Qg	4Qmax	5Qmin	6Vg	7mBase	8status	9Pmax	10Pmin
-gen_data = [gen_i gen_bus p_mw_ini mpc.gen(:,[PG QG PMIN PMAX QMIN QMAX GEN_STATUS]) v_set p_c2 p_c1 q_c2 q_c1 price_mode controllable priority s_max_mode d_mw_max];
+gen_data = [gen_i gen_bus p_mw_ini mpc.gen(:,[PG QG PMIN PMAX QMIN QMAX GEN_STATUS]) v_set p_c2 p_c1 q_c2 q_c1 controllable priority d_mw_max];
 
 %% Loads data creation
-load_headers = {'index', 'bus', 'load_type', 'is_active', 'controllable', 'priority', 'p_mw', 'q_mvar', 's_mode', 'p_mw_max', 'p_mw_min', 'q_mvar_max', 'q_mvar_min', 'flex_price_down', 'flex_price_up', 'flex_price_mode'};
+load_headers = {'index', 'connection1', 'is_active', 'controllable', 'priority', 'p_mw', 'q_mvar'};
 %load_i = (0:(sum(mpc.bus(:, PD) ~= 0) - 1)).';
 load_i = (0:(sum(mpc.bus(:, PD) + mpc.bus(:, QD) ~= 0) - 1)).';
 % load_name = "lcrit" + load_i;
 load_bus = ((mpc.bus(:, PD) + mpc.bus(:, QD) ~= 0).*mpc.bus(:, BUS_I));
 load_bus(load_bus == 0) = [];
-load_bus = load_bus -1;
-load_type = "critical" + strings(height(load_i), 1); % By default all loads are critical
 is_active = ones(height(load_i), 1); % By default all loads are active
 controllable = zeros(height(load_i), 1); % By default there are no loads controllable
 priority = ones(height(load_i), 1); % By default all loads have priority 1
@@ -128,39 +120,35 @@ p_mw = p_mw_temp;
 q_mvar = q_mvar_temp;
 p_mw((p_mw_temp + q_mvar_temp) == 0) = [];
 q_mvar(p_mw_temp + q_mvar_temp == 0) = [];
-s_mode = "fixed" + strings(height(load_i), 1); % By default all loads use fixed demand
-p_mw_max = zeros(height(load_i), 1); % By default 0
-p_mw_min = zeros(height(load_i), 1); % By default 0
-q_mvar_max = zeros(height(load_i), 1); % By default 0
-q_mvar_min = zeros(height(load_i), 1); % By default 0
-flex_price_down = zeros(height(load_i), 1); % By default 0
-flex_price_up = zeros(height(load_i), 1); % By default 0
-flex_price_mode = "fixed" + strings(height(load_i), 1); % By default fixed
-load_data = [load_i load_bus load_type is_active controllable priority -p_mw -q_mvar s_mode -p_mw_max -p_mw_min -q_mvar_max -q_mvar_min	flex_price_down flex_price_up flex_price_mode];
+load_data = [load_i load_bus is_active controllable priority -p_mw -q_mvar];
 
 %% Branchs data creation
-line_headers = {'index', 'from_bus', 'to_bus', 's_mva_max', 'is_active', 'r', 'x', 'g_sh', 'b_sh', 'ang_grad_min', 'ang_grad_max', 'overload_pu'};
+line_headers = {'index', 'from_node', 'to_node', 's_mva_max', 'is_active', 'r_ohm', 'x_ohm', 'b_c_mho', 'tap_ratio_from', 'phase_shift_from', 'tap_ratio_to', 'phase_shift_to', 'ang_grad_min', 'ang_grad_max', 'overload_pu'};
 line_i = (0:(height(mpc.branch) - 1)).';
 % line_name = "line" + line_i;
-from_bus = mpc.branch(:, F_BUS) - 1;
-to_bus = mpc.branch(:, T_BUS) - 1;
-s_max = mpc.branch(:, RATE_A) + (mpc.branch(:, RATE_A)==0)*1000;
-v_kv = mpc.bus((from_bus + 1), BASE_KV);
+from_bus = mpc.branch(:, F_BUS);
+to_bus = mpc.branch(:, T_BUS);
+s_max = mpc.branch(:, RATE_A) + (mpc.branch(:, RATE_A)==0)*max(mpc.branch(:, RATE_A))*1000;
+v_kv = mpc.bus((to_bus), BASE_KV);
 r = mpc.branch(:, BR_R).*v_kv.*v_kv/sn;
 x = mpc.branch(:, BR_X).*v_kv.*v_kv/sn;
-g_sh = line_i * 0;
+% g_sh = line_i * 0;
 %g_sh = mpc.branch(:, BR_G).*v_kv.*v_kv/sn
-b_sh = mpc.branch(:, BR_B).*v_kv.*v_kv/sn;
+b_c = mpc.branch(:, BR_B)./v_kv./v_kv*sn;
+tap_f = mpc.branch(:, TAP) + (mpc.branch(:, TAP)==0);
+shift_f = mpc.branch(:, SHIFT);
+tap_t = ones(height(line_i), 1);
+shift_t = zeros(height(line_i), 1);
 overload_pu = ones(height(line_i), 1)*0.8; % By default 0.8
 % pglib branch data structure
 %	1fbus	2tbus	3r	4x	5b	6rateA	7rateB	8rateC	9ratio	10angle	11status	12angmin	13angmax
-line_data = [line_i from_bus to_bus s_max mpc.branch(:,BR_STATUS) r x g_sh b_sh mpc.branch(:, [ANGMIN ANGMAX]) overload_pu];
+line_data = [line_i from_bus to_bus s_max mpc.branch(:,BR_STATUS) r x b_c tap_f shift_f tap_t shift_t mpc.branch(:, [ANGMIN ANGMAX]) overload_pu];
 
 
 %% Excel creation 
 if err == 0
-    writecell(config_headers,filename,'Sheet','config');
-    writecell(config_data,filename,'Sheet','config','Range','A2');
+    % writecell(config_headers,filename,'Sheet','config');
+    % writecell(config_data,filename,'Sheet','config','Range','A2');
     
     writecell(bus_headers,filename,'Sheet','Bus');
     writematrix(bus_data,filename,'Sheet','Bus','Range','A2');
@@ -171,8 +159,8 @@ if err == 0
     writecell(gen_headers,filename,'Sheet','Generator');
     writematrix(gen_data,filename,'Sheet','Generator','Range','A2');
     
-    writecell(load_headers,filename,'Sheet','Load');
-    writematrix(load_data,filename,'Sheet','Load','Range','A2');
+    writecell(load_headers,filename,'Sheet','ElectricLoad');
+    writematrix(load_data,filename,'Sheet','ElectricLoad','Range','A2');
     
     fprintf('EXCEL FILE %s SUCCESSFULY CREATED\n', filename)
 else
---
+++ playground.m
@@ -0,0 +1,8 @@
+clc
+clear all
+% mpc = loadcase('case118');
+pglib2excel('case118');
+% mpopt = mpoption('opf.flow_lim', 'S');
+% results = runpf('test');
+% runopf(mpc, mpopt, 'test.txt', 'test')
+% runopf(mpc)
